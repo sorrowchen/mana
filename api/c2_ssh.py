258da4efea239e68e07e12ae9b3f24eec7ff82ae
huming <mx.mean@gmail.com>
2014-08-15 19:37:41 +0800

    add key

--- api/c2_ssh.py
+++ api/c2_ssh.py
@@ -3,12 +3,18 @@
 import paramiko
 import json
 import os
+from StringIO import StringIO
+
+PUBLIC_KEY="""
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvRW4Zsw2fsNUEmHR/d3yaLC+5aqcDVrXZwtbGKr5jQalhuwdrezJxu7INXtuVa24o0S+0AxjXd9rdmKBAkR7e0OUgiAGm4HQEssA5iymKvqCcl+zVDUcytHSU87fG6JGuk5IJSdhxyvc2LclS5QZjvwZl/6DkrVQ/fslv1ekwYug6hbPoF8Ovvdi7dGvFZhw6i4alxvGuu3Mq1XsPZGG/fbQyK8gTsDZ6VjIoqZRP1D0TbXebz7S7CK1cYHJufKN6Q0WZALBIQI1u/XWoZdsRSqt/TAhkOUOg7S0jM8SSRO2JfRJbcx+iLbruFLj7kzgscBaWTgcG9lMyg3f5e8/qw== root@ypt_server10
+"""
 
 def conn(host,command,user="root",pwd=None,port=22):	
 	ssh=paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	print "host:%s,command:%s,user:%s,pwd:%s,port:%s" % (host,command,user,pwd,port)
-	ssh.connect(host,port,user,pwd)
+        key = paramiko.RSAKey.from_private_key(StringIO(PUBLIC_KEY), password=passphrase)
+	ssh.connect(host,port,user,pwd,pkey=key)
 	stdin,stdout,stderr=ssh.exec_command(command)
 	error=stderr.readlines()
 	output=stdout.readlines()
