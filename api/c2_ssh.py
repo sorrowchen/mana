7cac5e9332d5b0f3090100ac909068c579efb766
huming <mx.mean@gmail.com>
2014-08-15 20:22:43 +0800

    add key without pwd

--- api/c2_ssh.py
+++ api/c2_ssh.py
@@ -9,15 +9,9 @@ def conn(host,command,user="root",pwd=None,port=22):
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	print "host:%s,command:%s,user:%s,pwd:%s,port:%s" % (host,command,user,pwd,port)
 	PRIVATE_KEY='/root/.ssh/id_rsa'
-        #key = paramiko.RSAKey.from_private_key(PRIVATE_KEY,password=None)
-	#ssh.load_system_host_keys()
 	ssh.connect(host,port,user,pwd,key_filename=PRIVATE_KEY)
-	print "------start read----"
 	stdin,stdout,stderr=ssh.exec_command(command)
-	print "------read end-------"
-	print type(stderr)
 	error=stderr.readlines()
-	print type(error)
 	output=stdout.readlines()
 	ssh.close()
 	if not error:
