21d921848af704f884eaa1dd7003d8fd37ab416b
huming <mx.mean@gmail.com>
2014-08-15 20:12:27 +0800

    add key without pwd

--- api/c2_ssh.py
+++ api/c2_ssh.py
@@ -9,16 +9,16 @@ def conn(host,command,user="root",pwd=None,port=22):
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	print "host:%s,command:%s,user:%s,pwd:%s,port:%s" % (host,command,user,pwd,port)
 	PRIVATE_KEY='/root/.ssh/id_rsa'
-        key = paramiko.RSAKey.from_private_key(PRIVATE_KEY,password=None)
+        #key = paramiko.RSAKey.from_private_key(PRIVATE_KEY,password=None)
 	#ssh.load_system_host_keys()
-	ssh.connect(host,port,user,pwd,pkey=key)
+	ssh.connect(host,port,user,pwd)
 	print "------start read----"
 	stdin,stdout,stderr=ssh.exec_command(command)
 	print "------read end-------"
 	print type(stderr)
-	error=stderr.read()
+	error=stderr.readlines()
 	print type(error)
-	output=stdout.read()
+	output=stdout.readlines()
 	ssh.close()
 	if not error:
 	    ot_list=json.dumps(output)
@@ -28,11 +28,6 @@ def conn(host,command,user="root",pwd=None,port=22):
 	    errot_list=json.dumps(error)
 	    print "An error happened by:%s" % errot_list
 	    return "An _error_ happened by:%s" % errot_list
-
-
-def sudo_conn(host,cmd):
-    stream=os.popen("sudo python sudo_ssh.py '%s' %s" % (host,cmd)).read()
-    return stream
 
 
 
