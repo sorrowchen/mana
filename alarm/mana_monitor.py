3b4716c2c686e1361d222d0b09aeefbd6f8fef27
root <root@ywy.(none)>
2015-04-16 17:48:40 +0800

    fix some too long code

--- alarm/mana_monitor.py
+++ alarm/mana_monitor.py
@@ -175,9 +175,9 @@ def alarm(instance, data, alarm_obj, threshold, region):
 
 def _alarm(msg_body):
     try:
-        message = 'Region:%s\n Instance_ID:%s\n Instance_Name:%s\n Project:%s\n User:%s\n AlarmBody:%s\n Device:%s\n Message:data is to high %s %s\n'\
-                    %(msg_body.region, msg_body.instance_id, msg_body.instance_name, msg_body.project, msg_body.user, msg_body.alarm_obj,\
-                      msg_body.device_name, msg_body.max_data, msg_body.unit)
+        message = 'Region:%s\nInstance_ID:%s\nInstance_Name:%s\nProject:%s\nUser:%s\nAlarmBody:%s\nDevice:%s\ndata:%s%s\n'\
+                    %(msg_body.region, msg_body.instance_id, msg_body.instance_name, msg_body.project, msg_body.user, \
+                      msg_body.alarm_obj, msg_body.device_name, msg_body.max_data, msg_body.unit)
         LOG.info(message)
         send_email(msg_body)
         send_phone_message(msg_body)
@@ -191,7 +191,7 @@ def send_email(msg_body):
     sender_pwd = CONF.get('email_sender_pwd')
     receivers = CONF.get('email_receiver')
     try:
-        msg = "报警模块:%s<br> 异常数据值:%s%s<br> 报警区域:%s<br> 主机名:%s<br> 主机id:%s<br> 所属项目:%s<br> 所属用户:%s<br> 设备名称:%s<br>"\
+        msg = "报警模块:%s<br> 异常数据值:%s%s<br>报警区域:%s<br>主机名:%s<br>主机id:%s<br>所属项目:%s<br>所属用户:%s<br>设备名称:%s<br>"\
                     %(msg_body.alarm_obj, msg_body.max_data, msg_body.unit, msg_body.region, msg_body.instance_name,\
                       msg_body.instance_id, msg_body.project, msg_body.user, msg_body.device_name)
         subject = '星云监控报警' 
@@ -209,7 +209,8 @@ def send_phone_message(msg_body):
     acttype=CONF.get('phone_acttype')
     receivers=CONF.get('phone_receiver')
     try:
-        msg = "报警模块:%s,区域:%s,项目:%s,名称:%s,数据:%s%s" %(msg_body.alarm_obj, msg_body.region, msg_body.project, msg_body.device_name, msg_body.max_data, msg_body.unit)
+        msg = "报警模块:%s,区域:%s,项目:%s,名称:%s,数据:%s%s"%(msg_body.alarm_obj, msg_body.region,\
+                msg_body.project, msg_body.device_name, msg_body.max_data, msg_body.unit)
         for receiver in receivers:
             smsclient = SMS(phone_host, phone_port, gametype, priority, acttype)
             smsclient.sendmsg(receiver, msg)
